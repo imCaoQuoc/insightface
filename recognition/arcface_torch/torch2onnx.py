60f3aa82a91cda8c6931e754444b644d63a6b3b3
imCaoQuoc <caoquocworkandstudy@gmail.com>
2024-12-24 10:27:54 +0700

    Update arcface_torch/torch2onnx.py

--- recognition/arcface_torch/torch2onnx.py
+++ recognition/arcface_torch/torch2onnx.py
@@ -3,10 +3,10 @@ import onnx
 import torch
 
 
-def convert_onnx(net, path_module, output, opset=11, simplify=False):
+def convert_onnx(net, path_module, output, opset=20, simplify=False):
     assert isinstance(net, torch.nn.Module)
     img = np.random.randint(0, 255, size=(112, 112, 3), dtype=np.int32)
-    img = img.astype(np.float)
+    img = img.astype(np.float64)
     img = (img / 255. - 0.5) / 0.5  # torch style norm
     img = img.transpose((2, 0, 1))
     img = torch.from_numpy(img).unsqueeze(0).float()
@@ -17,7 +17,6 @@ def convert_onnx(net, path_module, output, opset=11, simplify=False):
     torch.onnx.export(net, img, output, input_names=["data"], keep_initializers_as_inputs=False, verbose=False, opset_version=opset)
     model = onnx.load(output)
     graph = model.graph
-    graph.input[0].type.tensor_type.shape.dim[0].dim_param = 'None'
     if simplify:
         from onnxsim import simplify
         model, check = simplify(model)
